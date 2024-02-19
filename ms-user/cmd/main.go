0ba32b87be1934bc37c3baf2176a57017207c41a
rizface <alfarizzi2711@gmail.com>
2024-02-19 21:14:24 +0700

    auto run migrations when app start

--- ms-user/cmd/main.go
+++ ms-user/cmd/main.go
@@ -2,11 +2,43 @@ package main
 
 import (
 	"context"
+	"database/sql"
+	"errors"
 	"log"
+	"os"
 
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"github.com/jackc/pgx/v5/stdlib"
 	"github.com/rizface/go-ms-systemd/ms-user/cmd/rest"
 	"github.com/rizface/go-ms-systemd/ms-user/database"
 )
+
+func runMigration(db *sql.DB) error {
+	driver, err := postgres.WithInstance(db, &postgres.Config{})
+	if err != nil {
+		return err
+	}
+
+	m, err := migrate.NewWithDatabaseInstance(
+		"file://database/migrations",
+		os.Getenv("SYSTEMD_DB_NAME"),
+		driver,
+	)
+	if err != nil {
+		return err
+	}
+
+	err = m.Up()
+	if err != nil && !errors.Is(err, migrate.ErrNoChange) {
+		return err
+	}
+
+	log.Println("✅ Success run migrations")
+
+	return nil
+}
 
 func main() {
 	ctx := context.Background()
@@ -16,10 +48,16 @@ func main() {
 		log.Fatalf("failed created db connection pool: %s", err)
 	}
 
+	db := stdlib.OpenDBFromPool(dbPool)
+
+	err = runMigration(db)
+	if err != nil {
+		log.Fatalf("failed run migrations: %v", err)
+	}
+
 	log.Println("success establish connection with database")
 
 	restServer := rest.NewServer(dbPool)
-
 	if err := restServer.Start(); err != nil {
 		log.Fatalf("failed start http server: %s", err)
 	}
--- ms-user/go.mod
+++ ms-user/go.mod
@@ -6,16 +6,26 @@ require (
 	github.com/go-chi/chi/v5 v5.0.11
 	github.com/go-ozzo/ozzo-validation v3.6.0+incompatible
 	github.com/golang-jwt/jwt/v5 v5.2.0
+	github.com/golang-migrate/migrate/v4 v4.17.0
 	github.com/google/uuid v1.6.0
+	github.com/jackc/pgx v3.6.2+incompatible
 	github.com/jackc/pgx/v5 v5.5.3
 	golang.org/x/crypto v0.19.0
 )
 
 require (
 	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2 // indirect
+	github.com/cockroachdb/apd v1.1.0 // indirect
+	github.com/gofrs/uuid v4.4.0+incompatible // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/jackc/fake v0.0.0-20150926172116-812a484cc733 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/puddle/v2 v2.2.1 // indirect
-	golang.org/x/sync v0.1.0 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	golang.org/x/sync v0.5.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 )
